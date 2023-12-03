a5ddab843a491c75148664a6e53dc9b8f933cdec
gabrielfcisco <gabrifran017@gmail.com>
2023-12-03 01:01:08 -0300

    day02: the that the day01

--- Day02/day02.py
+++ Day02/day02.py
@@ -6,38 +6,49 @@ colors = {
 
 def getColors(line):
 
-    sets = line.split(";")
+    sets = [color.replace(" ","").split(",") for colors in [line.split(";")] for color in colors]
 
-    # print(sets)
-    for i in sets:
-        i = i.replace(" ","")
-        i = i.split(",")
-        for j in i:
+    for turn in sets:
+        for cube in turn:
             for color in colors:
-                pos = j.find(color)
+                pos = cube.find(color)
 
                 if pos >=0:
-                    count = int(j[:pos])
+                    count = int(cube[:pos])
                     if count > colors[color]:return False
-
+                
     return True
 
-    
-        
+def getMin(line):
+    sets = [color.replace(" ","").split(",") for colors in [line.split(";")] for color in colors]
+    blue = 0
+    red = 0
+    green = 0
+
+    for turn in sets:
+        pos = [(color, index[:index.find(color)]) for color in colors for index in turn if index.find(color)>=0]
+        pos = dict(pos)
+        if 'blue' in pos:
+            blue = max(int(pos['blue']), blue)
+        if 'green' in pos:
+            green = max(int(pos['green']), green)
+        if 'red' in pos:
+            red = max(int(pos['red']), red)
+
+    return blue*green*red
 
 if __name__ == "__main__":
-    input = open("input.txt", "r")
+    fileInput = open("input.txt", "r")
 
-    gamesTested = []
-    for line in input:
-        # gamesTested = [[i[4:], getColors(i[1])] for i in (line.split(":"))]
-        games = line.split(":")
-        gamesTested.append([games[0][5:],getColors(games[1])])
-        print(gamesTested)
-    
-    totalSum = 0
-    for game in gamesTested:
-        if game[1]:
-            totalSum += int(game[0])
-    
-    print(totalSum)
+    #1st Part
+
+    gamesTested = [[games[0][5:], getColors(games[1])] for line in fileInput for games in [line.split(":")]]
+    totalSum = sum(int(game[0]) for game in gamesTested if game[1]==True)    
+    print(f'Soma dos valores: {totalSum}')
+
+    #2nd Part
+
+    fileInput.seek(0)
+    gamesTested = [getMin(games[1]) for line in fileInput for games in [line.split(":")]]
+    totalSum = sum(values for values in gamesTested)
+    print(f'Soma dos valores: {totalSum}')
