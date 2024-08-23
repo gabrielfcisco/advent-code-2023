d5d63e62f959feabf60683d5554b529103dd0492
Gabriel Francisco Santos Sousa <gabriel.sousa@saddc.cmu.unicamp.br>
2024-08-23 17:12:44 -0300

    i think i did it😭

--- Day05/day05.py
+++ Day05/day05.py
@@ -21,38 +21,41 @@ def convert_range(lines_to_convert: list, index_input: int, seeds: list):
 
     return dic_prev, index_input + index_convert + 2
 
-def convert_seeds(lines_to_convert: list, index_input: int, seeds: list):
+def convert_seeds(lines_to_convert: list, seeds: list):
     dic = []
     
-    for index_convert, line in enumerate(lines_to_convert):
+    while seeds:
 
-        line = line.strip()
-        print(index_convert)
-        if not line:
+        seed = seeds.pop(0)
+
+        for location in lines_to_convert.split('\n')[1:]:
+            l, r1, r2 = map(int, location.split(None))
+            diff = l - r1
+            
+            if seed[1] <= r1 or seed[0] >= r1+r2:
+                continue
+            if seed[0] < r1:
+                dic.append([seed[0], r1])
+                seed[0] = r1
+            if seed[1] > r1+r2:
+                dic.append([r1+r2, seed[1]])
+                seed[1] = r1+r2
+            dic.append([seed[0]+diff, seed[1]+diff])
             break
-        
-        range1 = [int(i) for i in line.split(None)]
-        print(range1)
-        for seed in seeds:
-            if seed[0] >= range1[1] and seed[1] <= range1[1]+range1[2]-1:
-                dif1 = seed[0]-range1[1]
-                dif2 = (range1[1]+range1[2]-1) - seed[1]
-                dic.append([range1[0]+dif1, (range1[0]+range1[2]-1)-dif2])
-            elif seed[0] < range1[1] and seed[1] <= range1[1]+range1[2]-1:
-                dif2 = (range1[1]+range1[2]-1) - seed[1]
-                dic.append([range1[0],(range1[0]+range1[2]-1)-dif2])
-            elif seed[0] >= range1[1] and seed[1] > range1[1]+range1[2]-1:
-                dif1 = seed[0]-range1[1] 
-                dic.append([range1[0]+dif1, range1[1]+range1[2]-1])
-        print(dic)
-    return dic, index_input + 2
-
+        else:
+            dic.append([seed[0], seed[1]])
+            
+   
+    return dic
+    
 if __name__ == "__main__":
-    with open("input.txt", "r") as fileInput:
-        fileInput = fileInput.readlines()
+    with open("input.txt", "r") as f:
+        fileInput = f.read()
+
+        lines = fileInput.split('\n\n')
 
         #Part 1:
-        seeds = list(map(int, fileInput[0].split(":")[1].split(None)))
+        seeds = list(map(int, lines[0].split(":")[1].split(None)))
 
         print(seeds)
 
@@ -85,5 +88,23 @@ if __name__ == "__main__":
 
         range_seeds = [[seeds[i],seeds[i]+seeds[i+1]-1] for i in range(0,len(seeds),2)]
         print(range_seeds)
-        seedToSoil, index = convert_seeds(fileInput[index:], 3, range_seeds)
-        print(seedToSoil)
+        seedsToSoil = convert_seeds(lines[1], range_seeds)
+        print(seedsToSoil)
+        soilToFertilizer = convert_seeds(lines[2], seedsToSoil)
+        print(soilToFertilizer)
+        fertilizerToWater = convert_seeds(lines[3], soilToFertilizer)
+        print(fertilizerToWater)
+        waterToLight = convert_seeds(lines[4], fertilizerToWater)
+        print(waterToLight)
+        lightToTemperature = convert_seeds(lines[5], waterToLight)
+        print(lightToTemperature)
+        temperatureToHumidity = convert_seeds(lines[6], lightToTemperature)
+        print(temperatureToHumidity)
+        humidityToLocation = convert_seeds(lines[7], temperatureToHumidity)
+        print(humidityToLocation)
+
+        minimum = float('inf')
+        for x in humidityToLocation:
+            minimum = min(x[0], minimum)
+
+        print(minimum)
